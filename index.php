836c95c2e13933d97a9b3286760804ce7ea0712d
Gaopalelwe10 <gmpetshane@gmail.com>
2021-12-10 22:33:52 +0200

    index.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-<?php
 
+
+<?php
+include "library/config.php";
+$postjson = json_decode(file_get_contents('php://input'), true);
+echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Review App</title>
-   
+
 </head>
 <body>
- <h2>Welcom to restaurant review app </h2>
+   <h2> welcome to Restaurant Review App</h2>
 </body>
-</html>
-
+</html>';
 ?>
