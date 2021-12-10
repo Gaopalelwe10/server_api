d1cbea17fec65ee50ffe38d3763587e49ef715f2
Gaopalelwe10 <gmpetshane@gmail.com>
2021-12-10 22:58:05 +0200

    second commit

--- library/config.php
+++ library/config.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 header("Content-Type: application/json; charset=utf-8");
 
 // define('DB_NAME', 'restaurant_db'); // DATABASE
-// define('DB_USER', 'root'); // ROOT DEFAULT MYSQL
+// define('DB_USER', 'root'); // ROOT DEFAULT MYSQL    // default
 // define('DB_PASSWORD', '');  // PASSOWORD
 // define('DB_HOST', 'localhost'); // LOCAL IF YOU USE LOCAL.
 
@@ -15,7 +15,7 @@ header("Content-Type: application/json; charset=utf-8");
 define('DB_NAME', 'bxf8bogp1xd9zayiu9gt'); // DATABASE
 define('DB_USER', 'ujyxodzxda8daezl'); // ROOT DEFAULT MYSQL
 define('DB_PASSWORD', 'YQH32yLYDck1DbmGhPEY');  // PASSOWORD
-define('DB_HOST', 'bxf8bogp1xd9zayiu9gt-mysql.services.clever-cloud.com'); // LOCAL IF YOU USE LOCAL.
+define('DB_HOST', 'bxf8bogp1xd9zayiu9gt-mysql.services.clever-cloud.com'); // Server
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
