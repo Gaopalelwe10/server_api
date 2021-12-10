405ac53f7d45c926dff55da9b38b184647ede734
Gaopalelwe10 <gmpetshane@gmail.com>
2021-12-10 21:56:10 +0200

    make it better

--- library/config.php
+++ library/config.php
@@ -6,6 +6,12 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=utf-8");
 
+// define('DB_NAME', 'restaurant_db'); // DATABASE
+// define('DB_USER', 'root'); // ROOT DEFAULT MYSQL
+// define('DB_PASSWORD', '');  // PASSOWORD
+// define('DB_HOST', 'localhost'); // LOCAL IF YOU USE LOCAL.
+
+
 define('DB_NAME', 'bxf8bogp1xd9zayiu9gt'); // DATABASE
 define('DB_USER', 'ujyxodzxda8daezl'); // ROOT DEFAULT MYSQL
 define('DB_PASSWORD', 'YQH32yLYDck1DbmGhPEY');  // PASSOWORD
