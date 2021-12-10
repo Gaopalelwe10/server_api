5924ab7e813424507b3e77a4174ea54084099ef2
Gaopalelwe <50743716+Gaopalelwe10@users.noreply.github.com>
2021-12-10 22:28:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drive to Web Hosting</title>
+    <title>Restaurant Review App</title>
    
 </head>
 <body>
- 
+ <h2>Welcom to restaurant review app </h2>
 </body>
 </html>
 
